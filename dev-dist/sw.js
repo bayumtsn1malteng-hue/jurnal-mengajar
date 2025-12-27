09a830b5932467a0e7f421705b3503810d42ffc3
Mitra Artikel <mitra.artikel@gmail.com>
2025-12-27 21:27:48 +0900

    fix: PengaturanPage.jsx

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.6upk1deguoo"
+    "revision": "0.i780he09jqc"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
